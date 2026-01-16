ab941d7a68b80ee8b08d66ae8877e246557a9592
Kuchizu <legolashacker0@gmail.com>
2026-01-16 15:00:35 +0300

    feat: add UserDetails and UserDetailsService

--- api-gateway/src/main/java/ru/ifmo/se/restaurant/gateway/entity/User.java
+++ api-gateway/src/main/java/ru/ifmo/se/restaurant/gateway/entity/User.java
@@ -1,18 +1,24 @@
 package ru.ifmo.se.restaurant.gateway.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table("users")
-public class User {
+public class User implements UserDetails {
     @Id
     private Long id;
     private String username;
@@ -22,4 +28,34 @@ public class User {
     private Boolean enabled;
     private LocalDateTime createdAt;
     private LocalDateTime lastLogin;
+
+    @Override
+    @Transient
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isEnabled() {
+        return enabled != null && enabled;
+    }
 }
